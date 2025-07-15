22f344f13988700ec02b6dae2debbddf13ee639b
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2025-07-15 14:13:34 +0900

    add:チェックインコントローラの基盤作成

--- backend/app/controllers/check_ins_controller.rb
+++ backend/app/controllers/check_ins_controller.rb
@@ -1,16 +1,7 @@
 class CheckInsController < ApplicationController
-  before_action :set_check_in, only: %i[ show update destroy ]
 
-  # GET /check_ins
-  def index
-    @check_ins = CheckIn.all
-
-    render json: @check_ins
-  end
-
-  # GET /check_ins/1
-  def show
-    render json: @check_in
+  def validate
+    # GPS / QRコードチェックイン検証
   end
 
   # POST /check_ins
@@ -22,20 +13,6 @@ class CheckInsController < ApplicationController
     else
       render json: @check_in.errors, status: :unprocessable_entity
     end
-  end
-
-  # PATCH/PUT /check_ins/1
-  def update
-    if @check_in.update(check_in_params)
-      render json: @check_in
-    else
-      render json: @check_in.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /check_ins/1
-  def destroy
-    @check_in.destroy!
   end
 
   private
